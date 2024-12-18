426f1ae8c89971658cb64fd33c5465c5ec4d6ea5
manisha Sasmitha <117644553+MANISHA-CMD@users.noreply.github.com>
2024-12-18 22:07:17 +0530

    🧚‍♂️DARK_QUEEN-V1🧚‍♂️

--- plugins/repo.js
+++ plugins/repo.js
@@ -14,30 +14,4 @@ whatsapp number:94721551183
 
 
 
-const {cmd , commands} = require('../command')
-
-cmd({
-    pattern: "repo",
-    desc: "repo the bot",
-    category: "main",
-    react: "📡",
-    filename: __filename
-},
-
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-
-let dec = `*📍REPO LINK ❤️‍🔥👇*
-
-🩷◦ https://github.com/MANISHA-CMD
-
-*©ᴅᴀʀᴋ_Qᴜᴇᴇɴ-ᴠ1 ᴄʀᴇᴀᴛᴇ ʙʏ ᴍᴀɴɪꜱʜᴀ ꜱᴀꜱᴍɪᴛʜᴀ*
-
-`
-await conn.sendMessage(from,{image:{url: `https://files.catbox.moe/v1k9r3.jpg`},caption:dec},{quoted:mek});
-
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+function _0x4e5b(_0x1dac35,_0x58a71a){const _0x89e2f=_0x89e2();return _0x4e5b=function(_0x4e5b5d,_0x1e1f2c){_0x4e5b5d=_0x4e5b5d-0xac;let _0x4e91a9=_0x89e2f[_0x4e5b5d];return _0x4e91a9;},_0x4e5b(_0x1dac35,_0x58a71a);}const _0x47a530=_0x4e5b;(function(_0x2e9cec,_0x2ce139){const _0x2b86a7=_0x4e5b,_0x1de7e4=_0x2e9cec();while(!![]){try{const _0x29caf2=parseInt(_0x2b86a7(0xb5))/0x1*(parseInt(_0x2b86a7(0xaf))/0x2)+parseInt(_0x2b86a7(0xbb))/0x3*(parseInt(_0x2b86a7(0xae))/0x4)+parseInt(_0x2b86a7(0xbe))/0x5*(-parseInt(_0x2b86a7(0xb1))/0x6)+-parseInt(_0x2b86a7(0xb7))/0x7+-parseInt(_0x2b86a7(0xb3))/0x8*(-parseInt(_0x2b86a7(0xb9))/0x9)+-parseInt(_0x2b86a7(0xad))/0xa*(-parseInt(_0x2b86a7(0xba))/0xb)+parseInt(_0x2b86a7(0xb4))/0xc*(-parseInt(_0x2b86a7(0xbf))/0xd);if(_0x29caf2===_0x2ce139)break;else _0x1de7e4['push'](_0x1de7e4['shift']());}catch(_0x19b7ea){_0x1de7e4['push'](_0x1de7e4['shift']());}}}(_0x89e2,0xab1c1));function _0x89e2(){const _0x3f62d3=['660742FkHvwU','sendMessage','2742kecmKK','log','4364400ugJaAo','108LbRDYy','4FRUUfy','*📍REPO\x20LINK\x20❤️‍🔥👇*\x0a\x0a🩷◦\x20https://github.com/MANISHA-CMD\x0a\x0a*©ᴅᴀʀᴋ_Qᴜᴇᴇɴ-ᴠ1\x20ᴄʀᴇᴀᴛᴇ\x20ʙʏ\x20ᴍᴀɴɪꜱʜᴀ\x20ꜱᴀꜱᴍɪᴛʜᴀ*\x0a\x0a','2325211mQYahH','repo\x20the\x20bot','9UYCZFv','11tYWhmf','3FhBsSm','repo','https://files.catbox.moe/v1k9r3.jpg','5795MxrYpS','1820377RIIqEX','../command','3082870hMWOkf','2590564TKrnSQ'];_0x89e2=function(){return _0x3f62d3;};return _0x89e2();}const {cmd,commands}=require(_0x47a530(0xac));cmd({'pattern':_0x47a530(0xbc),'desc':_0x47a530(0xb8),'category':'main','react':'📡','filename':__filename},async(_0x24cd5e,_0x12d193,_0x4aab4f,{from:_0x389740,quoted:_0x5d20f6,body:_0xe1d310,isCmd:_0x289a68,command:_0x529350,args:_0x4a78df,q:_0x1a9309,isGroup:_0x37f885,sender:_0x5f5c7c,senderNumber:_0x53ef1f,botNumber2:_0x490b5e,botNumber:_0x1b6e0d,pushname:_0x2566ae,isMe:_0x21d990,isOwner:_0x2ad37f,groupMetadata:_0x206266,groupName:_0x695850,participants:_0x2a66f2,groupAdmins:_0xe95db,isBotAdmins:_0x351534,isAdmins:_0x42f12b,reply:_0x2fef47})=>{const _0x509876=_0x47a530;try{let _0x33453b=_0x509876(0xb6);await _0x24cd5e[_0x509876(0xb0)](_0x389740,{'image':{'url':_0x509876(0xbd)},'caption':_0x33453b},{'quoted':_0x12d193});}catch(_0x2c1fda){console[_0x509876(0xb2)](_0x2c1fda),_0x2fef47(''+_0x2c1fda);}});
