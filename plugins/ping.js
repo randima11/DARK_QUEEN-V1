05094d7e1d8e7f5e04474a3968e260e212c95afa
manisha Sasmitha <117644553+MANISHA-CMD@users.noreply.github.com>
2024-12-18 21:21:36 +0530

    🧚‍♂️DARK_QUEEN-V1🧚‍♂️

--- plugins/ping.js
+++ plugins/ping.js
@@ -13,25 +13,4 @@ whatsapp number:94721551183
 
 
 
-const config = require('../config')
-const { cmd, commands } = require('../command')
-
-cmd({
-    pattern: "ping",
-    desc: "Check bot's response time.",
-    category: "main",
-    react: "🚀",
-    filename: __filename
-},
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '*ᴅᴀʀᴋ_Qᴜᴇᴇɴ-ᴠ1*' })
-        const endTime = Date.now()
-        const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `🚀 𝗽𝗶𝗻𝗴 : ${ping}ms` }, { quoted: message })
-    } catch (e) {
-        console.log(e)
-        reply(`${e}`)
-    }
-})
+function _0x4de0(_0x2cfbf0,_0x2d06ad){const _0x33e90d=_0x33e9();return _0x4de0=function(_0x4de015,_0x5cd6ed){_0x4de015=_0x4de015-0x7e;let _0x21a1b9=_0x33e90d[_0x4de015];return _0x21a1b9;},_0x4de0(_0x2cfbf0,_0x2d06ad);}const _0x517b73=_0x4de0;function _0x33e9(){const _0x3dbd21=['main','6072312TFGSCd','7388654fIKzyf','1067701WZkhuJ','9300294vGfrUC','sendMessage','7273413kovzJO','186190BsCgWD','*ᴅᴀʀᴋ_Qᴜᴇᴇɴ-ᴠ1*','log','ping','Check\x20bot\x27s\x20response\x20time.','now','2761EbAGPn','5INXTBT','🚀\x20𝗽𝗶𝗻𝗴\x20:\x20','../command','8BCyNxu','10862RoUaVb','465pcMekA'];_0x33e9=function(){return _0x3dbd21;};return _0x33e9();}(function(_0x1a2616,_0x3cf943){const _0x4130b5=_0x4de0,_0x2e9542=_0x1a2616();while(!![]){try{const _0x20c690=-parseInt(_0x4130b5(0x88))/0x1+parseInt(_0x4130b5(0x83))/0x2*(-parseInt(_0x4130b5(0x84))/0x3)+parseInt(_0x4130b5(0x86))/0x4+parseInt(_0x4130b5(0x7f))/0x5*(-parseInt(_0x4130b5(0x89))/0x6)+-parseInt(_0x4130b5(0x87))/0x7*(parseInt(_0x4130b5(0x82))/0x8)+-parseInt(_0x4130b5(0x8b))/0x9+-parseInt(_0x4130b5(0x8c))/0xa*(-parseInt(_0x4130b5(0x7e))/0xb);if(_0x20c690===_0x3cf943)break;else _0x2e9542['push'](_0x2e9542['shift']());}catch(_0x181092){_0x2e9542['push'](_0x2e9542['shift']());}}}(_0x33e9,0xd3f75));const config=require('../config'),{cmd,commands}=require(_0x517b73(0x81));cmd({'pattern':_0x517b73(0x8f),'desc':_0x517b73(0x90),'category':_0x517b73(0x85),'react':'🚀','filename':__filename},async(_0x386ef3,_0x8cda9f,_0x423ca2,{from:_0x30d3dc,quoted:_0x554dd4,body:_0xcb9d26,isCmd:_0xb1dd4c,command:_0x304d8f,args:_0x2be720,q:_0x3406bc,isGroup:_0x129b35,sender:_0x20af48,senderNumber:_0x252ba3,botNumber2:_0xccc2e9,botNumber:_0x3eab6c,pushname:_0x4d4fb3,isMe:_0x4cba9b,isOwner:_0x484601,groupMetadata:_0x56bc6d,groupName:_0x2a4ad1,participants:_0x571b19,groupAdmins:_0x2cdc1d,isBotAdmins:_0x3ae85c,isAdmins:_0x53604a,reply:_0x2ebead})=>{const _0x41faa6=_0x517b73;try{const _0x152673=Date[_0x41faa6(0x91)](),_0x3f367f=await _0x386ef3[_0x41faa6(0x8a)](_0x30d3dc,{'text':_0x41faa6(0x8d)}),_0x38877c=Date[_0x41faa6(0x91)](),_0x4ce9f8=_0x38877c-_0x152673;await _0x386ef3['sendMessage'](_0x30d3dc,{'text':_0x41faa6(0x80)+_0x4ce9f8+'ms'},{'quoted':_0x3f367f});}catch(_0xc30042){console[_0x41faa6(0x8e)](_0xc30042),_0x2ebead(''+_0xc30042);}});
