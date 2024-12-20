bedbdd19de7d27e68349160cd7e226c4790b6b05
manisha Sasmitha <117644553+MANISHA-CMD@users.noreply.github.com>
2024-12-20 09:07:32 +0530

    Create all3.js

---
+++ plugins/all3.js
@@ -0,0 +1,61 @@
+const config = require('../config')
+const l = console.log
+const { cmd, commands } = require('../command')
+const dl = require('@bochilteam/scraper')  
+const ytdl = require('yt-search');
+const fs = require('fs-extra')
+var videotime = 60000 // 1000 min
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+cmd({
+    pattern: "yts",
+    alias: ["ytsearch"],
+    use: '.yts sameer kutti',
+    react: "🔎",
+    desc: "Search and get details from youtube.",
+    category: "search",
+    filename: __filename
+
+},
+
+async(conn, mek, m,{from, l, quoted, body, isCmd, umarmd, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if (!q) return reply('*Please give me words to search*')
+try {
+let yts = require("yt-search")
+var arama = await yts(q);
+} catch(e) {
+    l(e)
+return await conn.sendMessage(from , { text: '*Error !!*' }, { quoted: mek } )
+}
+var mesaj = '';
+arama.all.map((video) => {
+mesaj += ' *🖲️' + video.title + '*\n🔗 ' + video.url + '\n\n'
+});
+await conn.sendMessage(from , { text:  mesaj }, { quoted: mek } )
+} catch (e) {
+    l(e)
+  reply('*Error !!*')
+}
+});
+
+
+
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "ai",
+    alias: ["gpt","bot"], 
+    react: "📑",
+    desc: "ai chat.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+return reply(`${data.data}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
